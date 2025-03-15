7c6d6c29bdc5a4bd83b6c7b856f801a12bfc87c3
Gabe Witcher <gabewitcher@gmail.com>
2025-03-15 14:58:37 -0700

    Initial Commit

---
+++ mathTutor/mathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathTutorApp.swift
+//  mathTutor
+//
+//  Created by Gabe Witcher on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct mathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
